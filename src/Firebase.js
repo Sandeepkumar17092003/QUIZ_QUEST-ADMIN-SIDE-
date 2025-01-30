32308e0d358644fed004a1ff5070fd10109d9b46
Sandeepkumar Rai <142883385+Sandeepkumar17092003@users.noreply.github.com>
2025-01-30 21:08:27 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -10,13 +10,13 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5yYy-qCg_rfLx6gwFWMosAwafJPaj6cM",
-  authDomain: "crud-c1e4e.firebaseapp.com",
-  projectId: "crud-c1e4e",
-  storageBucket: "crud-c1e4e.appspot.com",
-  messagingSenderId: "722831696367",
-  appId: "1:722831696367:web:2a3199637d80b481d4961a",
-  measurementId: "G-MXXBTDH0WT"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
